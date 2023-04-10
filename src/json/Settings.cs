6c7e46e215700e163acd976e45a6da6170184c34
Kun Patrik <kunpatrik00@mailbox.unideb.hu>
2023-04-10 13:51:41 +0200

    git hates case sensitivity i guess

--- src/json/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TiktokBot.Json
-{
-    public struct Settings
-    {
-        public string BackgroundVideoPath { get; set; }
-        public string SubredditUrlOrFileName { get; set; }
-        public int NumberOfVidsPerPost { get; set; }
-    }
-}
--- src/resources/clear.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-rmdir muxed /S /Q
-rmdir images /S /Q
-rmdir videos /S /Q
-rmdir sounds /S /Q
-rmdir done /S /Q
-rmdir logs /S /Q
